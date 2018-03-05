355e6aac1443bbc3526979c7d545fc618daa9b93
Peter Joseph <contact@peterajoseph.com>
2018-03-06 07:10:53 +0800

    Added config and environment files

---
+++ config.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+
+let config = {
+
+	database: {
+		host: process.env.DB_HOST,
+		username: process.env.DB_USER,
+		password: process.env.DB_PASS,
+	}
+
+};
+
+module.exports = config;
