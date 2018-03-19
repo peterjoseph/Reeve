fac1c5505780275c655987997a163855bbf968a7
Peter Joseph <contact@peterajoseph.com>
2018-03-19 08:01:30 +0800

    Added basic i18n translation

--- client/app.js
+++ client/app.js
@@ -1,10 +1,13 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { I18n } from "react-i18next";
+import i18n from "./common/translations/i18n";
+
 import "./common/styles/entry.scss";
 
 class App extends React.Component {
   render() {
-    return <div>Root Component</div>;
+    return <I18n>{t => <div>Root Component {t("name.label")}</div>}</I18n>;
   }
 }
 
---
+++ client/common/translations/i18n.js
@@ -0,0 +1,15 @@
+import i18n from "i18next";
+
+i18n.init({
+  interpolation: {
+    escapeValue: false
+  },
+  lng: "en",
+  resources: {
+    en: {
+      translation: {}
+    }
+  }
+});
+
+export default i18n;
