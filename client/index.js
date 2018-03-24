b9f1d357af2e68eba521e543656827d1a1da86cc
Peter Joseph <contact@peterajoseph.com>
2018-03-24 18:34:02 +0800

    Added react-tooltip and notify-toast to project

--- client/index.js
+++ client/index.js
@@ -1,8 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { I18nextProvider } from "react-i18next";
 import i18next from "./common/translations/i18n";
+import Notifications from "react-notify-toast";
+import ReactTooltip from "react-tooltip";
+
 import { Provider } from "react-redux";
+import { I18nextProvider } from "react-i18next";
 
 import App from "./app";
 import store from "./common/store/store";
@@ -12,7 +15,11 @@ import "./common/styles/entry.scss";
 ReactDOM.render(
   <Provider store={store}>
     <I18nextProvider i18n={i18next}>
-      <App />
+      <div>
+        <Notifications />
+        <App />
+        <ReactTooltip />
+      </div>
     </I18nextProvider>
   </Provider>,
   document.getElementById("app")
