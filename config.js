cd5019b3361bd5cb29c7bf887e1bb67a77d4ee43
Peter Joseph <contact@peterajoseph.com>
2018-03-25 08:41:30 +0800

    Added more build properties

--- config.js
+++ config.js
@@ -1,6 +1,17 @@
 require("dotenv").config();
 
 let config = {
+  build: {
+    environment: process.env.ENVIRONMENT
+  },
+  development: {
+    protocol: process.env.PROTOCOL,
+    frontendServer: process.env.FRONTENDSERVER,
+    frontendPort: process.env.FRONTENDPORT,
+    backendServer: process.env.BACKENDSERVER,
+    backendPort: process.env.BACKENDPORT
+  },
+  environment: process.env.ENVIRONMENT,
   database: {
     host: process.env.DB_HOST,
     username: process.env.DB_USER,
