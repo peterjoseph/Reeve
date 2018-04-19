99ce59304ebcb07cbb387479e2a0c8a1fe39b129
Peter Joseph <contact@peterajoseph.com>
2018-04-20 06:50:55 +0800

    Updated project file structure

--- client/common/store/reducers/root.js
+++
@@ -1,8 +0,0 @@
-import { fromJS } from "immutable";
-
-export default function root(state = fromJS({}), action) {
-  if (action.entities) {
-    return state.merge(action.entities);
-  }
-  return state;
-}
--- shared/constants.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  ROLE_TYPE: Object.freeze({
-    UNREGISTERED: 0,
-    ROOT: 1,
-    ADMINISTRATOR: 2
-  })
-};
