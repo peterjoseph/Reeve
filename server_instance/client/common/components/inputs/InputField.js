abbe4dcd9327fdfaa1a6f917cd78b7679b346d47
Peter Joseph <contact@peterajoseph.com>
2018-04-23 20:48:10 +0800

    New InputField component

---
+++ server_instance/client/common/components/inputs/InputField.js
@@ -0,0 +1,21 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+class InputField extends React.Component {
+  render() {
+    const { error } = this.props;
+
+    return (
+      <div className="input-field">
+        <input>input component</input>
+        {error & <div>error</div>}
+      </div>
+    );
+  }
+}
+
+InputField.propTypes = {
+  error: PropTypes.string
+};
+
+export default InputField;
