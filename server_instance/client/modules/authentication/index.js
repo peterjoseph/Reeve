927f890570cbee143ada30948b66948d1c8df5e7
Peter Joseph <contact@peterajoseph.com>
2018-05-03 19:40:19 +0800

    Fixed style class not loading correctly

--- server_instance/client/modules/authentication/index.js
+++ server_instance/client/modules/authentication/index.js
@@ -6,15 +6,15 @@ import InputField from "../../common/components/inputs/InputField";
 class Authentication extends React.Component {
   render() {
     return (
-      <div id="authentication container-fluid h-100">
-        <div class="row justify-content-center h-100">
+      <div className="authentication container-fluid h-100">
+        <div className="row justify-content-center h-100 nopadding">
           <div className="form-container col-4 hidden-md-down">
             <div className="justify-content-center">
               <InputField />
               <InputField />
             </div>
           </div>
-          <div className="background-container col-10 col-sm-10 col-md-10 col-lg-8 col-xl-8">
+          <div className="background-container col-10 col-sm-10 col-md-10 col-lg-8 col-xl-8 nopadding">
             Right most column
           </div>
         </div>
