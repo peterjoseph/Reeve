8c21126a9ef999a77a1fb774974d5f411b1c87ed
Peter Joseph <contact@peterajoseph.com>
2018-05-03 20:23:41 +0800

    Added props to input field component

--- server_instance/client/common/components/inputs/InputField.js
+++ server_instance/client/common/components/inputs/InputField.js
@@ -3,11 +3,18 @@ import PropTypes from "prop-types";
 
 class InputField extends React.Component {
   render() {
-    const { error } = this.props;
+    const { label, id, type, ariaLabel, placeholder, error } = this.props;
 
     return (
-      <div className="input-field">
-        <input />
+      <div className="form-group">
+        {label && <label htmlFor={id}>{label}</label>}
+        <input
+          type={type}
+          className={"form-control"}
+          id={id}
+          aria-describedby={ariaLabel}
+          placeholder={placeholder}
+        />
         {error && <div>error</div>}
       </div>
     );
@@ -15,6 +22,11 @@ class InputField extends React.Component {
 }
 
 InputField.propTypes = {
+  label: PropTypes.string,
+  id: PropTypes.string,
+  type: PropTypes.string,
+  ariaLabel: PropTypes.string,
+  placeholder: PropTypes.string,
   error: PropTypes.string
 };
 
--- server_instance/client/modules/authentication/index.js
+++ server_instance/client/modules/authentication/index.js
@@ -6,16 +6,24 @@ import InputField from "../../common/components/inputs/InputField";
 class Authentication extends React.Component {
   render() {
     return (
-      <div className="authentication container-fluid h-100">
-        <div className="row justify-content-center h-100 nopadding">
-          <div className="form-container col-4 hidden-md-down">
-            <div className="justify-content-center">
-              <InputField />
-              <InputField />
+      <div id="authentication">
+        <div className="container-fluid h-100">
+          <div className="row justify-content-center h-100 nopadding">
+            <div className="form-container col-5 hidden-md-down">
+              <div className="p-5 justify-content-center">
+                <form>
+                  <InputField
+                    label={"Username"}
+                    id={"username-input"}
+                    type={"textField"}
+                    ariaLabel={"Username"}
+                    placeholder={"Username"}
+                  />
+                  <InputField />
+                </form>
+              </div>
             </div>
-          </div>
-          <div className="background-container col-10 col-sm-10 col-md-10 col-lg-8 col-xl-8 nopadding">
-            Right most column
+            <div className="background-container col-7 nopadding" />
           </div>
         </div>
       </div>
