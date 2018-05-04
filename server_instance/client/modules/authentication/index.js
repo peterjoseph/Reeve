ae6ba54f84dfc82ecae6fdb97ff87d94e59ca933
Peter Joseph <contact@peterajoseph.com>
2018-05-04 22:01:02 +0800

    Continuing to develop registration page

--- server_instance/client/modules/authentication/index.js
+++ server_instance/client/modules/authentication/index.js
@@ -10,16 +10,69 @@ class Authentication extends React.Component {
         <div className="container-fluid h-100">
           <div className="row justify-content-center h-100 nopadding">
             <div className="form-container col-5 hidden-md-down">
-              <div className="p-5 justify-content-center">
-                <form>
+              <div className="pl-5 pr-5 align-vertical justify-content-center">
+                <form className="w-100">
+                  <div className="w-100 mb-3">
+                    <span className="h3">Register</span>
+                  </div>
+                  <div className="form-row">
+                    <div className="col">
+                      <InputField
+                        label={"First Name"}
+                        id={"firstName-input"}
+                        type={"textField"}
+                        ariaLabel={"firstName"}
+                      />
+                    </div>
+                    <div className="col">
+                      <InputField
+                        label={"Last Name"}
+                        id={"lastName-input"}
+                        type={"textField"}
+                        ariaLabel={"lastName"}
+                      />
+                    </div>
+                  </div>
                   <InputField
-                    label={"Username"}
-                    id={"username-input"}
+                    label={"Email Address"}
+                    id={"email-input"}
                     type={"textField"}
-                    ariaLabel={"Username"}
-                    placeholder={"Username"}
+                    ariaLabel={"emailAddress"}
                   />
-                  <InputField />
+                  <InputField
+                    label={"Password"}
+                    id={"password-input"}
+                    type={"password"}
+                    ariaLabel={"Password"}
+                  />
+                  <div className="form-group">
+                    <label htmlFor={"domain-input"}>{"Workspace URL"}</label>
+                    <div class="input-group">
+                      <input
+                        id="domain-input"
+                        className={"form-control"}
+                        type="text"
+                        placeholder="companyname"
+                      />
+                      <div class="input-group-append">
+                        <div class="input-group-text">.domainname.com</div>
+                      </div>
+                    </div>
+                  </div>
+                  <button
+                    type="button"
+                    class="btn btn-primary btn-lg btn-block mt-4 p-3"
+                  >
+                    Sign Up
+                  </button>
+                  <div className="text-center">
+                    <span>
+                      <small>
+                        By signing up, you agree to our Terms & Conditions and
+                        Privacy policy
+                      </small>
+                    </span>
+                  </div>
                 </form>
               </div>
             </div>
