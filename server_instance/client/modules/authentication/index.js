eb51cc7a85e5ebb22d2078cbcf33d36521157178
Peter Joseph <contact@peterajoseph.com>
2018-05-11 23:04:14 +0800

    Cleaning up divs

--- server_instance/client/modules/authentication/index.js
+++ server_instance/client/modules/authentication/index.js
@@ -8,17 +8,17 @@ class Authentication extends React.Component {
   render() {
     return (
       <div id="authentication">
-        <div className="container-fluid h-100">
-          <div className="row justify-content-center h-100 nopadding">
-            <div className="form-container col-5 d-flex flex-column hidden-md-down">
-              <div className="w-100 pl-5 pr-5 border-bottom">
-                Login | Register
-              </div>
-              <div className="h-100 d-flex justify-content-center align-items-center">
+        <div className="background-container nopadding">
+          <div className="container-fluid">
+            <div className="row justify-content-center nopadding">
+              <div className="form-container col-5 d-flex flex-column hidden-md-down">
+                <div className="w-100 pl-5 pr-5 border-bottom">
+                  Login | Register
+                </div>
                 <Register />
               </div>
+              <div className="col-7" />
             </div>
-            <div className="background-container col-7 nopadding" />
           </div>
         </div>
       </div>
