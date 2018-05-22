37db3dfbedbdf9ee9da5202cc1d3388e64d36715
Peter Joseph <contact@peterajoseph.com>
2018-05-22 21:33:59 +0800

    Async perform database updates for client registration

--- server_instance/server/routes/authentication.js
+++ server_instance/server/routes/authentication.js
@@ -2,15 +2,15 @@ import { register } from "~/shared/validation/authentication";
 import { t } from "~/shared/translations/i18n";
 import { perform } from "../database.js";
 import validate from "validate.JS";
+import async from "async";
 
-// Register New Client Account
 module.exports = function(router) {
-	// Attempt to create client account
 	// Attempt to create new owner level user
-	// Pass and throw 'err'
 	// Report to Sentry
 	// Report to Papertrail
 	// Return securityKey
+
+	// Register New Client Account
 	router.post("/internal/register", function(req, res) {
 		// Store received object properties
 		const received = {
@@ -25,27 +25,49 @@ module.exports = function(router) {
 		if (valid != null) {
 			res.status(403).send({ message: t("validation.clientInvalidProperties"), errors: valid });
 		}
-		// Connect to Database to perform existing Workspace URL Lookup
+		// Perform database connection
 		perform().getConnection(function(err, connection) {
-			if (err) throw err;
-			// Check Workspace URL is unique in database
-			connection.query("SELECT workspaceURL FROM `client` WHERE `workspaceURL` = ?", [req.body.workspaceURL], function(error, results, fields) {
-				if (error) throw error;
-				// Send error if workspace url is taken
-				if (results != null && results.length > 0) {
+			// Return error if database connection error
+			if (err) {
+				res.status(500);
+			}
+			// Async update properties in database
+			async.series(
+				[
+					function(chain) {
+						// Check if workspaceURL is already in use
+						connection.query("SELECT workspaceURL FROM `client` WHERE `workspaceURL` = ?", [req.body.workspaceURL], function(error, results, fields) {
+							if ((results != null && results.length > 0) || error) {
+								// Pass through error object if failure
+								const errorObj = { status: 403, message: t("validation.clientInvalidProperties"), errors: error || valid };
+								chain(errorObj, null);
+							} else {
+								chain(null, results);
+							}
+						});
+					},
+					function(chain) {
+						// Create clientObject and insert new row in the client table
+						const clientObject = { name: received.workspaceURL, workspaceURL: received.workspaceURL, createdDate: new Date(), modifiedDate: new Date() };
+						connection.query("INSERT INTO client SET ?", clientObject, function(error, results, fields) {
+							if (error) {
+								chain(error, null);
+							} else {
+								chain(null, results);
+							}
+						});
+					}
+				],
+				function(error, data) {
+					// Close our connection regardless of success or failure
 					connection.release();
-					return res.status(403).send({ message: t("validation.clientInvalidProperties"), errors: valid });
+					if (error) {
+						res.status(error.status).send({ ...error });
+					} else {
+						res.status(200).send({ status: 200, message: t("label.success") });
+					}
 				}
-			});
-			// Create client row in database
-			const clientObject = { name: received.workspaceURL, workspaceURL: received.workspaceURL, createdDate: new Date(), modifiedDate: new Date() };
-			connection.query("INSERT INTO client SET ?", clientObject, function(error, results, fields) {
-				if (error) throw error;
-				// Release connection
-				connection.release();
-				// Return successful server response
-				return res.status(200);
-			});
+			);
 		});
 	});
 };
