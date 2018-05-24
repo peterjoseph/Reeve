5c2be8f612b6068acde7dae35011027d853929bc
Peter Joseph <contact@peterajoseph.com>
2018-05-24 19:16:27 +0800

    New roles tables sql

---
+++ databases/MySQL/scripts/180524_01.sql
@@ -0,0 +1,30 @@
+INSERT INTO `executedScripts` (`name`, `description`, `createdDate`, `executedDate`)
+VALUES
+	('180524_01', 'Create new role tables and foreign keys', NOW(), NOW());
+
+CREATE TABLE `roles` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL DEFAULT '',
+  `description` varchar(255) NOT NULL DEFAULT '',
+  `createdDate` datetime NOT NULL,
+  `modifiedDate` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+INSERT INTO `roles` (`id`, `name`, `description`, `createdDate`, `modifiedDate`)
+VALUES
+	(1, 'Owner', 'Highest level role with access to all subscription features', NOW(), NOW());
+
+CREATE TABLE `userRoles` (
+  `id` int(21) unsigned NOT NULL AUTO_INCREMENT,
+  `userId` int(11) unsigned NOT NULL,
+  `roleId` int(11) unsigned NOT NULL,
+  `active` tinyint(1) NOT NULL DEFAULT '1',
+  `createdDate` datetime NOT NULL,
+  `modifiedDate` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_userId` (`userId`),
+  KEY `FK_roleId` (`roleId`),
+  CONSTRAINT `FK_roleId` FOREIGN KEY (`roleId`) REFERENCES `roles` (`id`),
+  CONSTRAINT `FK_userId` FOREIGN KEY (`userId`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
