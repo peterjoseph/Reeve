4cc463a75be9c9881db20b6a9618015cf013825c
Peter Joseph <contact@peterajoseph.com>
2018-06-02 04:24:31 +0800

    Cleaned up client table and modified default subscription type

---
+++ databases/MySQL/scripts/180602_01.sql
@@ -0,0 +1,18 @@
+INSERT INTO `executedScripts` (`name`, `description`, `createdDate`, `executedDate`)
+VALUES
+	('180602_01', 'Clean up client table and organize subscriptions', NOW(), NOW());
+
+ALTER TABLE `client`
+DROP COLUMN `trial`,
+DROP COLUMN `trialExpiry`,
+DROP COLUMN `subscription`;
+
+ALTER TABLE `client`
+ADD COLUMN `subscriptionEndDate` date DEFAULT NULL;
+
+ALTER TABLE `client`
+MODIFY COLUMN `subscriptionId` int(3) DEFAULT 1;
+
+UPDATE `subscriptions`
+SET `name` = 'Trial', `description` = 'Default Trial account when a new client is created'
+WHERE id = 1;
