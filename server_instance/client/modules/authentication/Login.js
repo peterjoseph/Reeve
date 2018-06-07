705b8a0abd46f62d892932a426f85c1ea7e1cccf
Peter Joseph <contact@peterajoseph.com>
2018-06-08 07:54:25 +0800

    Added client specific styling to login page

--- server_instance/client/modules/authentication/Login.js
+++ server_instance/client/modules/authentication/Login.js
@@ -3,6 +3,9 @@ import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 import validate from "validate.JS";
+import { css } from "emotion";
+
+import { REDUX_STATE } from "~/shared/constants";
 import { t } from "~/shared/translations/i18n";
 import { extractSubdomain } from "~/shared/utilities/subdomain";
 
@@ -69,15 +72,57 @@ class Login extends Component {
 		const { emailAddress, password, keepSignedIn, loading, errors } = this.state;
 		const { workspaceURLStatus, clientStyle } = this.props;
 
+		// Don't return the page if workspaceURL state is still pending
+		if (workspaceURLStatus == null || workspaceURLStatus == REDUX_STATE.PENDING) {
+			return <div />;
+		}
+
+		// Handle client specific page styling
+		let clientLoginButton,
+			clientLinks,
+			clientRightColumn = "";
+		if (workspaceURLStatus == REDUX_STATE.FULFILLED && clientStyle != null && clientStyle.size > 0) {
+			clientLoginButton = css`
+				&,
+				&:hover,
+				&:active,
+				&:visited,
+				&:focus {
+					background-color: ${clientStyle.get("primaryColor")} !important;
+					border-color: ${clientStyle.get("primaryColor")} !important;
+				}
+				&:hover {
+					opacity: 0.9;
+				}
+			`;
+			clientLinks = css`
+				a,
+				a:active,
+				a.visited {
+					color: ${clientStyle.get("primaryColor")};
+				}
+				a:hover {
+					color: ${clientStyle.get("primaryColor")};
+				}
+			`;
+			clientRightColumn = css(
+				Object.assign(
+					{},
+					clientStyle.get("backgroundColor") != null && { backgroundColor: clientStyle.get("backgroundColor") },
+					clientStyle.get("backgroundImage") && { backgroundImage: `url('${clientStyle.get("backgroundImage")}')` }
+				)
+			);
+		}
+
 		return (
 			<Fragment>
-				<div className="form-container col-xs-12 col-md-6 col-lg-5 d-flex flex-column hidden-md-down">
+				<div className={`form-container col-xs-12 col-md-6 col-lg-5 d-flex flex-column hidden-md-down ${clientLinks}`}>
 					<div id="login">
 						<div className="p-3 p-sm-5 align-vertical justify-content-center">
 							<form className="w-100">
 								<div className="w-100 text-center mb-4">
-									<span>
-										<img src={require("../../common/images/logo_small.png")} />
+									<span className="logo">
+										<img src={clientStyle.get("logoImage") || require("../../common/images/logo_small.png")} />
 									</span>
 								</div>
 								<div className="w-100 mb-3">
@@ -122,7 +167,7 @@ class Login extends Component {
 									</div>
 									<div className="col text-right">{t("components.authentication.forgotPassword")}</div>
 								</div>
-								<button type="button" className="btn btn-primary btn-lg btn-block mt-4 p-3" onClick={this.login} disabled={loading}>
+								<button type="button" className={`btn btn-primary btn-lg btn-block mt-4 p-3 ${clientLoginButton}`} onClick={this.login} disabled={loading}>
 									{t("action.login")}
 								</button>
 								<div className="mt-4">
@@ -132,7 +177,7 @@ class Login extends Component {
 						</div>
 					</div>
 				</div>
-				<div className="col-md-6 col-lg-7" />
+				<div className={`background-container col-md-6 col-lg-7 ${clientRightColumn}`} />
 			</Fragment>
 		);
 	}
