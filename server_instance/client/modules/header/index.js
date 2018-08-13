e8d15138a249955906ff91085e424615a4a98300
Peter Joseph <contact@peterajoseph.com>
2018-08-14 06:26:20 +1000

    Bootstrap header

--- server_instance/client/modules/header/index.js
+++ server_instance/client/modules/header/index.js
@@ -16,11 +16,44 @@ class Header extends Component {
 		return (
 			<Fragment>
 				<VerifyEmail user={user} />
-				<div className="navbar navbar-inverse nav navbar-dark bg-dark">
-					<div className="navbar-inner">
-						<div className="container" />
-					</div>
-				</div>
+				<nav className="navbar navbar-expand-lg navbar-light bg-light p-3">
+					<a className="navbar-brand" href="#">
+						Navbar
+					</a>
+					<ul className="navbar-nav">
+						<li className="nav-item active">
+							<a className="nav-link" href="#">
+								Home <span className="sr-only">(current)</span>
+							</a>
+						</li>
+						<li className="nav-item">
+							<a className="nav-link" href="#">
+								Features
+							</a>
+						</li>
+						<li className="nav-item">
+							<a className="nav-link" href="#">
+								Pricing
+							</a>
+						</li>
+						<li className="nav-item dropdown">
+							<a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+								Dropdown link
+							</a>
+							<div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+								<a className="dropdown-item" href="#">
+									Action
+								</a>
+								<a className="dropdown-item" href="#">
+									Another action
+								</a>
+								<a className="dropdown-item" href="#">
+									Something else here
+								</a>
+							</div>
+						</li>
+					</ul>
+				</nav>
 			</Fragment>
 		);
 	}
