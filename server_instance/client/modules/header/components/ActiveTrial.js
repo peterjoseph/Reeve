82bf1166b8de322c51a7a5541e1cc01508f46620
Peter Joseph <contact@peterajoseph.com>
2018-08-25 15:25:58 +1000

    ActiveTrial component complete

--- server_instance/client/modules/header/components/ActiveTrial.js
+++ server_instance/client/modules/header/components/ActiveTrial.js
@@ -1,17 +1,32 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
+import moment from "moment";
 
+import { t } from "shared/translations/i18n";
+import { SUBSCRIPTION_TYPE, FEATURES } from "shared/constants";
+import { arrayContains } from "shared/utilities/filters";
 import User from "common/components/User";
 
 class ActiveTrial extends Component {
 	render() {
 		const { user } = this.props;
 
+		// Hide component if following requirements are not met
+		// Billing Feature, Trial Subscription
+		if (user.get("subscriptionId") !== SUBSCRIPTION_TYPE.TRIAL || !arrayContains(FEATURES.BILLING, user.get("clientFeatures").toJS() || [])) {
+			return null;
+		}
+
+		// Calculate days left in trial
+		const startDate = moment(user.get("subscriptionStartDate"));
+		const endDate = moment(user.get("subscriptionEndDate"));
+		const trialDaysLeft = startDate.diff(endDate, "days");
+
 		return (
 			<li className="nav-item mx-2 d-none d-md-inline-block">
 				<Link to={{ pathname: "/billing" }} className={"nav-link m-1"}>
-					<span className="badge badge-danger p-2">9 Days left on Trial</span>
+					<span className="badge badge-danger p-2">{t("components.billing.trialDaysLeft", { count: trialDaysLeft })}</span>
 				</Link>
 			</li>
 		);
