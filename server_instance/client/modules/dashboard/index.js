eaac959ecfa09fd2e9758a823c887242a7c474b9
Peter Joseph <contact@peterajoseph.com>
2018-09-04 08:04:52 +1000

    Updated dashboard page with e with icon

--- server_instance/client/modules/dashboard/index.js
+++ server_instance/client/modules/dashboard/index.js
@@ -1,11 +1,36 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 
+import { t } from "shared/translations/i18n";
+
 import User from "common/components/User";
 
 class Dashboard extends Component {
 	render() {
-		return <div>Dashboard</div>;
+		return (
+			<div className="container">
+				<div className="px-3 py-3 pt-md-5 pb-md-4 mx-auto">
+					<div className="mb-3 text-center">
+						<h4>{t("label.dashboard")}</h4>
+						<div>
+							<svg
+								width="64px"
+								height="64px"
+								fill="none"
+								stroke="currentColor"
+								strokeLinecap="round"
+								strokeLinejoin="round"
+								strokeWidth="2"
+								viewBox="0 0 24 24"
+								xmlns="http://www.w3.org/2000/svg"
+							>
+								<polyline points="22 12 18 12 15 21 9 3 6 12 2 12" />
+							</svg>
+						</div>
+					</div>
+				</div>
+			</div>
+		);
 	}
 }
 
