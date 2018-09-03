31cfade12f39b80ea0a79563d1ab21d8590ee8c6
Peter Joseph <contact@peterajoseph.com>
2018-09-04 07:00:31 +1000

    Added countries and currencies mappings to translation folder

--- server_instance/shared/translations/i18n.js
+++ server_instance/shared/translations/i18n.js
@@ -4,12 +4,14 @@ import markdownJsx from "i18next-markdown-jsx-plugin";
 const languages = {
 	en: {
 		translation: require("./languages/en.json"),
-		links: require("./links/en.json")
+		links: require("./links/en.json"),
+		countries: require("./countries/en.json"),
+		currencies: require("./currencies/en.json")
 	}
 };
 
 i18next.use(markdownJsx).init({
-	ns: ["translation", "links"],
+	ns: ["translation", "links", "countries", "currencies"],
 	interpolation: {
 		escapeValue: false
 	},
@@ -23,10 +25,22 @@ i18next.use(markdownJsx).init({
 
 export default i18next;
 
+// Standard text string translations
 export function t(...args) {
 	return i18next.t(...args);
 }
 
+// Links and URLS
 export function l(link) {
 	return i18next.t(`links:${link}`);
+}
+
+// ISO2 to country name mappings
+export function co(ISO2) {
+	return i18next.t(`countries:${ISO2}`);
+}
+
+// ISO2 to currencies mappings
+export function cu(ISO2) {
+	return i18next.t(`currencies:${ISO2}`);
 }
