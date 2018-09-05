bfba1a8344314fdbb42ad0c1cee09f9c52ef5129
Peter Joseph <contact@peterajoseph.com>
2018-09-06 06:52:16 +1000

    Disable react and redux dev tools in production

--- server_instance/client/common/store/store.js
+++ server_instance/client/common/store/store.js
@@ -3,6 +3,12 @@ import thunk from "redux-thunk";
 
 import root from "./reducers/root";
 
+// Determine if React development tools should be enabled or disabled
+if (process.env.NODE_ENV === "production" && window.__REACT_DEVTOOLS_GLOBAL_HOOK__ && Object.keys(window.__REACT_DEVTOOLS_GLOBAL_HOOK__._renderers).length) {
+	window.__REACT_DEVTOOLS_GLOBAL_HOOK__._renderers = {};
+}
+
+// Determine if Redux development tools should be enabled or disabled
 const composeEnhancers = (process.env.NODE_ENV !== "production" && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
 const store = createStore(root, composeEnhancers(applyMiddleware(thunk)));
 
