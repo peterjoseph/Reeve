ee5ae2c79e07414d9347a249947a5ccccf917e34
Peter Joseph <contact@peterajoseph.com>
2018-09-13 07:08:55 +1000

    Second attempt at updating packages

--- server_instance/webpack.config.js
+++ server_instance/webpack.config.js
@@ -71,10 +71,10 @@ module.exports = {
 			{
 				test: /.jsx?$/,
 				loader: "babel-loader",
-				exclude: /node_modules/,
 				query: {
 					presets: ["es2015", "stage-0", "react"]
-				}
+				},
+				exclude: path.resolve(__dirname, "node_modules/")
 			},
 			{
 				test: /\.css$/,
