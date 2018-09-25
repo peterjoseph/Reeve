55df1af1382f05084d97a1c719e36c4ee334c685
Peter Joseph <contact@peterajoseph.com>
2018-09-26 07:32:03 +1000

    Flag icons

---
+++ server_instance/client/common/media/icons/flags/Italy.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const ItalyFlagIcon = props => {
+	return (
+		<svg {...props} viewBox="0 0 640 480">
+			<g fillRule="evenodd" strokeWidth="1pt">
+				<path d="M0 0h640v480H0z" fill="#fff" />
+				<path d="M0 0h213.3v480H0z" fill="#009246" />
+				<path d="M426.7 0H640v480H426.7z" fill="#ce2b37" />
+			</g>
+		</svg>
+	);
+};
+
+export default ItalyFlagIcon;
--- server_instance/client/modules/authentication/components/LanguageSwitcher.js
+++ server_instance/client/modules/authentication/components/LanguageSwitcher.js
@@ -1,12 +1,17 @@
 import React, { Component } from "react";
 
+import EnglandFlagIcon from "common/media/icons/flags/England";
+import ItalyFlagIcon from "common/media/icons/flags/Italy";
+
 class LanguageSwitcher extends Component {
 	render() {
 		return (
 			<div className="row mt-4 justify-content-center">
 				<div className="col-xs-2 col-centered">
 					<select className="form-control">
-						<option value="english">English</option>
+						<option value="english">
+							<EnglandFlagIcon width="16px" height="16px" />
+						</option>
 						<option value="italian">Italian</option>
 					</select>
 				</div>
