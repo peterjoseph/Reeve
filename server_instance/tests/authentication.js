0a5d7cee87efe7f8422879b21f9b67f98e74d479
Peter Joseph <contact@peterajoseph.com>
2018-10-10 19:57:56 +1100

    Set environmental variable in testing so webpack doesn't break

--- server_instance/tests/authentication.js
+++ server_instance/tests/authentication.js
@@ -1,9 +1,17 @@
-import test from "ava";
-const request = require("supertest");
-const server = require("../server/server.js");
+// Set NODE_ENV to "development" because AVA changes it to "test" which breaks Webpack
+process.env.NODE_ENV = "development";
+
+// Import AVA
+const test = require("ava");
+
+// connect to the server
+const app = require("../server/server.js");
+
+// create a new session using supertest
+const request = require("supertest").agent(app.listen());
 
 test("example", async t => {
-	const response = await request(server).get("/status");
+	const response = await request(request).get("/status");
 	t.is(response.status, 200);
 	t.deepEqual(response.body, {
 		status: "Ok"
