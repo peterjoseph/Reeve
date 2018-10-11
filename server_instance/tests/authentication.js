1d8f84a7a5ca51ddb0d24843b16843cf96d37c07
Peter Joseph <contact@peterajoseph.com>
2018-10-11 23:41:44 +1100

    Validate Workspace URL 403 test

--- server_instance/tests/authentication.js
+++ server_instance/tests/authentication.js
@@ -1,11 +1,21 @@
 const test = require("ava");
 const app = require("../server/server.js");
-const server = app.listen();
-const request = require("supertest").agent(server);
+const config = require("../config");
+let request = require("supertest");
 
-test("Status Check", t => {
-	const response = request.get("/status");
-	t.is(response.status, 200);
+// Launch Server
+const server = app.listen();
+
+// Define default server path
+request = request(config.build.publicPath);
+
+/******** AUTHENTICATION TEST CASES ********/
+
+// Workspace URL
+// 403 Error Response when missing or incorrect workspace url provided
+test("Workspace URL - 403", async t => {
+	const response = await request.get("internal/validate_workspace_url/");
+	t.is(response.status, 403);
 });
 
 // Exit test server on completion
