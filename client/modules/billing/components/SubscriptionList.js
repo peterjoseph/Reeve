a6c55d42aae531ba643704c1863c05d5cc9847e9
Peter Joseph <contact@peterajoseph.com>
2018-12-24 13:39:10 +0800

    Updated text labels on billing page

--- client/modules/billing/components/SubscriptionList.js
+++ client/modules/billing/components/SubscriptionList.js
@@ -3,74 +3,88 @@ import PropTypes from "prop-types";
 import { connect } from "react-redux";
 // import { bindActionCreators } from "redux";
 import { withRouter } from "react-router-dom";
+import moment from "moment";
 
 // import { } from "common/store/reducers/billing.js";
-
+import { SUBSCRIPTION_TYPE } from "shared/constants";
+import { t } from "shared/translations/i18n";
 import User from "common/components/User";
 
 class SubscriptionList extends Component {
+	trialDays() {
+		let trialDaysLeft = 0;
+		if (this.props.user.get("subscriptionId") == SUBSCRIPTION_TYPE.TRIAL) {
+			const currentDate = moment(this.props.user.get("loginTime"));
+			const endDate = moment(this.props.user.get("subscriptionEndDate"));
+			trialDaysLeft = endDate.diff(currentDate, "days");
+		}
+		return trialDaysLeft;
+	}
+
 	render() {
+		const trialDaysLeft = this.trialDays(); // Calculate days left in trial
+
 		return (
 			<div className="container">
 				<div className="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
-					<h1 className="display-4">Pricing</h1>
-					<p className="lead">You are not yet a customer, please select a subscription from below.</p>
+					<h1>{t("components.billing.timeRemaining", { count: trialDaysLeft })}</h1>
+					<p className="lead">{t("components.billing.selectPlan")}</p>
 				</div>
 				<div className="card-deck mb-3 text-center">
 					<div className="card mb-4 shadow-sm">
 						<div className="card-header">
-							<h4 className="my-0 font-weight-normal">Basic</h4>
+							<h4 className="my-0 font-weight-normal">{t("label.basic")}</h4>
 						</div>
 						<div className="card-body">
 							<h1 className="card-title pricing-card-title">
-								$0 <small className="text-muted">/ mo</small>
+								$9 <small className="text-muted text-lowercase">/ {t("label.month")}</small>
 							</h1>
 							<ul className="list-unstyled mt-3 mb-4">
-								<li>10 users included</li>
-								<li>2 GB of storage</li>
-								<li>Email support</li>
-								<li>Help center access</li>
+								<li>Unique Feature 1</li>
+								<li>Unique Feature 2</li>
+								<li>Unique Feature 3</li>
+								<li>Unique Feature 4</li>
 							</ul>
 							<button type="button" className="btn btn-block btn-primary">
-								Get started
+								{t("components.billing.chooseThisPlan")}
 							</button>
 						</div>
 					</div>
 					<div className="card mb-4 shadow-sm">
 						<div className="card-header">
-							<h4 className="my-0 font-weight-normal">Professional</h4>
+							<h4 className="my-0 font-weight-normal">{t("label.standard")}</h4>
 						</div>
 						<div className="card-body">
 							<h1 className="card-title pricing-card-title">
-								$15 <small className="text-muted">/ mo</small>
+								$15 <small className="text-muted text-lowercase">/ {t("label.month")}</small>
 							</h1>
 							<ul className="list-unstyled mt-3 mb-4">
-								<li>20 users included</li>
-								<li>10 GB of storage</li>
-								<li>Priority email support</li>
-								<li>Help center access</li>
+								<li>Unique Feature 1</li>
+								<li>Unique Feature 2</li>
+								<li>Unique Feature 3</li>
+								<li>Unique Feature 4</li>
 							</ul>
 							<button type="button" className="btn btn-block btn-primary">
-								Get started
+								{t("components.billing.chooseThisPlan")}
 							</button>
 						</div>
 					</div>
 					<div className="card mb-4 shadow-sm">
 						<div className="card-header">
-							<h4 className="my-0 font-weight-normal">Enterprise</h4>
+							<h4 className="my-0 font-weight-normal">{t("label.professional")}</h4>
 						</div>
 						<div className="card-body">
 							<h1 className="card-title pricing-card-title">
-								$29 <small className="text-muted">/ mo</small>
+								$29 <small className="text-muted text-lowercase">/ {t("label.month")}</small>
 							</h1>
 							<ul className="list-unstyled mt-3 mb-4">
-								<li>30 users included</li>
-								<li>15 GB of storage</li>
-								<li>Phone and email support</li>
-								<li>Help center access</li>
+								<li>Unique Feature 1</li>
+								<li>Unique Feature 2</li>
+								<li>Unique Feature 3</li>
+								<li>Unique Feature 4</li>
 							</ul>
 							<button type="button" className="btn btn-block btn-primary">
-								Contact us
+								{t("components.billing.chooseThisPlan")}
 							</button>
 						</div>
 					</div>
@@ -91,4 +105,11 @@ function mapDispatchToProps(dispatch) {
 	};
 }
 
-export default withRouter(User(connect(null, mapDispatchToProps)(SubscriptionList)));
+export default withRouter(
+	User(
+		connect(
+			null,
+			mapDispatchToProps
+		)(SubscriptionList)
+	)
+);
