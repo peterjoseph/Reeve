102104ec92594fe4c045730755b595abdb6a3de5
Peter Joseph <contact@peterajoseph.com>
2018-12-31 22:17:21 +0800

    List of available subscriptions returned

--- server/orchestrator/billing.js
+++ server/orchestrator/billing.js
@@ -1,5 +1,6 @@
 import { database, models } from "services/sequelize";
 import { t } from "shared/translations/i18n";
+import { ServerResponseError } from "utilities/errors/serverResponseError";
 
 // Load client subscription details
 export function loadClientSubscriptionDetails(browserLng) {
@@ -20,8 +21,21 @@ export function loadClientSubscriptionDetails(browserLng) {
 export function loadAvailableSubscriptions(browserLng) {
 	return database().transaction(async function(transaction) {
 		try {
+			// Load a list of available stripe plans
+			const loadPlans = await models().plans.findAll(
+				{ where: { newSubscriptionsAllowed: true, active: true }, attributes: { exclude: ["createdAt", "updatedAt", "newSubscriptionsAllowed", "active"] } },
+				{ transaction: transaction }
+			);
+
+			// Throw an error if no stripe plans are available
+			if (loadPlans === null) {
+				throw new ServerResponseError(403, t("error.somethingWentWrong", { lng: browserLng }), null);
+			}
+
+			const plans = JSON.parse(JSON.stringify(loadPlans));
+
 			// Create a response object
-			const response = { status: 200, message: t("label.success", { lng: browserLng }) };
+			const response = { status: 200, message: t("label.success", { lng: browserLng }), subscriptions: plans };
 
 			// Return the response object
 			return response;
