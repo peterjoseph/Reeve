9a2c907d9b089290aa8bfc653758f479ce456a73
Peter Joseph <contact@peterajoseph.com>
2019-04-16 16:15:29 +0800

    Fixed wrong translation string loading on language change in Header component

--- client/common/components/User.js
+++ client/common/components/User.js
@@ -1,8 +1,12 @@
 import { connect } from "react-redux";
 import { AUTHENTICATION } from "../store/reducers/authentication.js";
+import { LANGUAGE } from "../store/reducers/language.js";
 
 const fetchUserProperties = connect(state => ({
-	user: state.getIn([AUTHENTICATION, "user", "payload"])
+	user: state.getIn([AUTHENTICATION, "user", "payload"]),
+	userStatus: state.getIn([AUTHENTICATION, "user", "status"]),
+	language: state.getIn([LANGUAGE, "changeLanguage", "payload"]),
+	languageStatus: state.getIn([LANGUAGE, "changeLanguage", "status"])
 }));
 
 export default function User(WrappedComponent) {
